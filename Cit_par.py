fcc3e2fcf6c92d559f68129e08d4fedbac9fe279
rwvos <61152818+rwvos@users.noreply.github.com>
2020-03-23 11:10:00 +0100

    Update Cit_par.py

--- Cit_par.py
+++ Cit_par.py
@@ -35,13 +35,13 @@ T_x_th = np.array([[1, 0, 0], [0, cos(th0), sin(th0)], [0, -sin(th0), cos(th0)]]
 m      =      1000       # mass [kg]
 
 # aerodynamic properties
-e      = 0.8         # Oswald factor [ ]
-CD0    = 0.04        # Zero lift drag coefficient [ ]
-CLa    = 5.084       # Slope of CL-alpha curve [ ]
+e      = 0.7764550889925007         # Oswald factor [ ]
+CD0    = 0.021969748034172783       # Zero lift drag coefficient [ ]
+CLa    = 4.684502932989611      # Slope of CL-alpha curve [ /rad]
 
 # Longitudinal stability
-Cma    = -0.5626     # longitudinal stabilty [ ]
-Cmde   = -1.1642     # elevator effectiveness [ ]
+Cma    = -0.58256477    # longitudinal stabilty [ ]
+Cmde   = -1.21166653     # elevator effectiveness [ ]
 
 # Aircraft geometry
 S      = 30.00	          # wing area [m^2]
