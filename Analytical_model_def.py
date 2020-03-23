605ba159e50351dd540547e678a87c6c0c7210de
xvanbeurden <57659530+xvanbeurden@users.noreply.github.com>
2020-03-23 22:41:34 +0100

    Waarden printen

--- Analytical_model_def.py
+++ Analytical_model_def.py
@@ -152,7 +152,7 @@ def dutchroll(V0, m, rho, muc, mub, CL, CD, CX0, CZ0):
     C_dutch = 4 * mub * Cnb + CYb * Cnr
 
     dutch_char = np.poly1d([A_dutch, B_dutch, C_dutch])
-    lambda_b_dutch = np.roots(dutch_char) #misschien V0/b toevoegen eraan
+    lambda_b_dutch = np.roots(dutch_char) * V0/b
     period_dutch = 2 * np.pi / abs(lambda_b_dutch[0].imag) * b/V0
     t_half_dutch = np.log(0.5) / lambda_b_dutch[0].real * b/V0
     zeta_dutch = -lambda_b_dutch[0].real / np.sqrt((lambda_b_dutch[0].imag)**2+(lambda_b_dutch[0].real)**2)
@@ -169,3 +169,19 @@ def dutchroll_2(V0, m, rho, muc, mub, CL, CD, CX0, CZ0):
     lambda_b_dutch2_1 = (-B_dutch2 + np.sqrt(4 * A_dutch2 * C_dutch2 - B_dutch2 **2)) / (2 * A_dutch2) * V0/b
     lambda_b_dutch2_2 = (-B_dutch2 - np.sqrt(4 * A_dutch2 * C_dutch2 - B_dutch2 **2)) / (2 * A_dutch2) * V0/b
     return lambda_b_dutch2_1, lambda_b_dutch2_2
+
+print()
+print("Short period motion:")
+print(short_period(V0_spm, m_spm, rho_spm, muc_spm, mub_spm, CL_spm, CD_spm, CX0_spm, CZ0_spm))
+print()
+print("Phugoid: ")
+print(short_period(V0_phug, m_phug, rho_phug, muc_phug, mub_phug, CL_phug, CD_phug, CX0_phug, CZ0_phug))
+print()
+print("A-periodic roll")
+print(aperiodic_roll(V0_apr, m_apr, rho_apr, muc_apr, mub_apr, CL_apr, CD_apr, CX0_apr, CZ0_apr))
+print()
+print("Spiral")
+print(spiral(V0_spir, m_spir, rho_spir, muc_spir, mub_spir, CL_spir, CD_spir, CX0_spir, CZ0_spir))
+print()
+print("Dutch roll")
+print(dutchroll(V0_dutch, m_dutch, rho_dutch, muc_dutch, mub_dutch, CL_dutch, CD_dutch, CX0_dutch, CZ0_dutch))
